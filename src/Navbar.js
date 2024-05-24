bde403b8c771958d270658c637f604318ea3c260
katja-droid <sosnyukk@gmail.com>
2024-05-24 03:09:28 +0300

    finalornotohn

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src="/logo.png" alt="Logo" /></Link>
+                <Link className="navbar-brand" to="/usercourses"><img src="/logo.png" alt="Logo" /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
